a7c07dd7ab08a8fb6e6c69d7d146734f8d1ad978
NotTh3ta <yash.patel@mail.concordia.ca>
2023-01-21 19:39:57 -0500

    Merge branch 'main' of https://github.com/volovikariel/ConUHacks23

--- popup.js
+++ popup.js
@@ -29,8 +29,7 @@ input.addEventListener('input',function(e){
         improveB.style.visibility ='visible';
     }
     if (input.value === "1234"){
-        //answer.innerText= "Your password sucks!";
-        answer.innerText=generatePassword(3);
+        answer.innerText= "Your password sucks!";
     }
     
 });
@@ -41,3 +40,67 @@ if(autocomplete!=null){
 
 //How safe is password?
 
+
+fetch('./SafetyTable.json')
+.then(response => response.json())
+.then(data => {table=data})
+ function howsafe( password){
+    
+    //Figure out which categorie the password is in
+    index = 0;
+    if(containsOnlyNumbers(password)) index = 1;
+    else if(containsLowercase(password)&& !containsUppercase(password)&& !containsSpecialchar(password) && !containsNumbers(password)) index = 2;
+    else if (containsLowercase(password)&& containsUppercase(password)&& !containsSpecialchar(password)&& !containsNumbers(password)) index = 3;
+    else if(containsLowercase(password)&& containsUppercase(password)&& !containsSpecialchar(password)&& containsNumbers(password)) index =4;
+    else if (containsLowercase(password)&& containsUppercase(password)&& containsSpecialchar(password)&& containsNumbers(password)) index =5;
+    else index =1;
+
+    console.log("Index is"+index);
+    //Parse password to string to get its length
+    password = password.toString();
+    l =  password.length-4; 
+    if(l>14) l = 14;
+    console.log("Length is"+index);
+
+    switch (index) {
+        case 1:
+            return table[l]['Numbers Only']
+            break;
+        case 2:
+            return table[l]['Lowercase Letters']
+            break;
+        case 3:
+            return table[l]['Upper and Lowercase Letters']
+           
+            break;
+        case 4:
+            return table[l]['Numbers, Upper and Lowercase Letters']
+            
+            break;
+        case 5:
+            return table[l]['Numbers, Upper and Lowercase Letters, Symbols']
+            break;
+    
+        default:
+            return "Couldn't calculate O-O' "
+            break;
+    }
+
+}
+
+function containsOnlyNumbers(password) {
+    return /^\d+$/.test(password);
+  }
+  function containsNumbers(str){
+    return /\d/.test(str);
+  }
+function containsUppercase(str) {
+    return /[A-Z]/.test(str);
+  }
+function containsLowercase(str) {
+    return /[a-z]/.test(str);
+  }
+var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+function containsSpecialchar(str){
+    return format.test(str);
+}
