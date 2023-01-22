e9924884b3f16c7b92bab6072c627f5fcde5cad6
NotTh3ta <yash.patel@mail.concordia.ca>
2023-01-22 08:34:48 -0500

    Fixed password gen

--- generatePassword.js
+++ generatePassword.js
@@ -106,12 +106,7 @@ function generatePassword() {
   if (addSpecialChar) {
     password += specialChars.charAt(randInt(0, specialChars.length - 1));
   }
-
-  password = "";
-  for(let i = 0; i < chosenWords.length; i++){
-    password += chosenWords[i];
-  }
-  updateImage(chosenWords);
+  
   return password;
 }
 
