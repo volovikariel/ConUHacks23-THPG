e3251ac3d37d90e0dfa6b8662b244d806def8789
NotTh3ta <yash.patel@mail.concordia.ca>
2023-01-21 20:58:33 -0500

    changed comments to pass generator

--- generatePassword.js
+++ generatePassword.js
@@ -8,22 +8,22 @@ fetch('./eff_short_wordlist.json')
 function generatePassword(n){
     let password = "";
     for(let i=0; i<n; i++){
-        password += wordsJSON[diceRollInt()];
+        password += wordsJSON[diceRollInt(4)];
     }   
     return password;
 }
 
 //Random int based on size n of dict
-function diceRollInt(){
+function diceRollInt(n){
     let sum = 0;
     for(let i=0; i<4; i++){
-        sum += Math.pow(10,i)*randInt(1,7);
+        sum += Math.pow(10,i)*randInt(1,n+1);
     }
     return sum;
 }
 
-function randInt(min,max){
-    let val = Math.floor((Math.random())*(max-min)+min);
+function randInt(max){
+    let val = Math.floor((Math.random())*(max-1)+1);
     return val;
 }
 
